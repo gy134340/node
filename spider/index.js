4101fc7dc1ae004108ed4b398de5362c120db791
gy134340 <guoyang134340@163.com>
2017-02-12 16:34:13 +0800

    general spider

--- spider/index.js
+++ spider/index.js
@@ -1,23 +1,46 @@
+/**
+ * my node spider
+ * @author  gy
+ */
+
 var request = require('superagent');
 var cheerio = require('cheerio');
 var express = require('express');
 var url = require('url');
 var mysql = require('mysql');
+var async = require('async');
 
 var app = express();
 var connection;
 var baseUrl = 'https://www.douban.com/group/shanghaizufang/discussion?start=0';
+var urls = [];
+for(var i = 0; i < 100; i++){
 
-request.get(baseUrl).end(function (err, res) {
-	if(err){
-		return console.log('error ', err);
-	}
+	// note: do more here
+	urls.push(baseUrl);
+}
+console.log('urls',urls);
 
-	analyze(res);
+// 异步抓取
+async.mapLimit(urls, 4, function (url, callback){
+	getHtml(url, callback);
+},function (err, result) {
+	console.log(err);
 });
 
+// note: 需要传入calback
+function getHtml (url, callback) {
+	request.get(url).end(function (err, res) {
+		if(err){
+			return console.log('error ', err);
+		}
 
-// @param response
+		analyze(res);
+		callback(null, url + ' html content');
+	});
+}
+
+// @param response html
 function analyze (res) {
 	var dt = [];
 	var $ = cheerio.load(res.text);
@@ -29,16 +52,19 @@ function analyze (res) {
 			href:href,
 			title:title,
 			date:date
-		}
+		};
 		dt.push(obj);
 	});
 	console.log('urls', dt);
-	insert(dt);
+
+
+	// 查询
+	connect (myInsert, dt);
 	
 }
 
 // mysql connect
-function connect () {
+function connect (func, dt) {
 	connection = mysql.createConnection({
 		host:'localhost',
 		user:'root',
@@ -54,13 +80,15 @@ function connect () {
 		console.log('connection success');
 	});
 
-	// connection.end();
+	func(dt);
+
+	connection.end();
 	
 }
 
 // insert
-function insert (dt) {
-	connect();
+function myInsert (dt) {
+	// connect();
 	for(var i = 0;i < dt.length; i++){
 		var query = "insert into douban (href,title,time) values ("+connection.escape(dt[i].href)+","+connection.escape(dt[i].title)+","+connection.escape(dt[i].date)+");";
 
@@ -68,15 +96,35 @@ function insert (dt) {
 		  if (error){
 		  	console.log(error);
 		  	return;
-		  };
+		  }
 
-		  console.log(query);
+		  // console.log(query);
 		});
 	}
 
-	connection.end();
+	// connection.end();
 }
 
+function myDelete () {
+
+}
+
+
+function mySelect () {
+	var query = 'select * from douban where 1';
+	connection.query(query, function (error, results, fields) {
+		  if (error){
+		  	console.log(error);
+		  	return;
+		  }
+
+		  console.log('results',results);
+		});
+}
+
+function myUpdate () {
+
+}
 
 
 
